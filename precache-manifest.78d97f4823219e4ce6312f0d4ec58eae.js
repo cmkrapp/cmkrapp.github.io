96ac6dc381823a55a120ba155f16ba8c3b12655c
cmkrapp <87389672+cmkrapp@users.noreply.github.com>
2021-07-14 00:41:10 +0600

    Add files via upload

---
+++ precache-manifest.78d97f4823219e4ce6312f0d4ec58eae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fa6b764f503a83bfc18af02c5701ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6bc00866dc4f592abb4",
+    "url": "./static/css/2.18f36260.chunk.css"
+  },
+  {
+    "revision": "338b1a18e72683ae519e",
+    "url": "./static/css/main.7da4def8.chunk.css"
+  },
+  {
+    "revision": "b6bc00866dc4f592abb4",
+    "url": "./static/js/2.d89af2a1.chunk.js"
+  },
+  {
+    "revision": "670195e9dae107f19334753ff3b65e09",
+    "url": "./static/js/2.d89af2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338b1a18e72683ae519e",
+    "url": "./static/js/main.029b40fe.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
