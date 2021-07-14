4c218750bbc99341d7684db077c3cc8e7b8645b4
CoinMaker <87389672+cmkrapp@users.noreply.github.com>
2021-07-14 20:31:52 +0600

    Add files via upload

---
+++ precache-manifest.ce24a0ddc636e9ce884992cc3ae9b8ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d55fd4655987041acd27cdb022a96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fcf320b5f87808a14cf",
+    "url": "/static/css/2.18f36260.chunk.css"
+  },
+  {
+    "revision": "2c61b0a9e2f08e67f9a5",
+    "url": "/static/css/main.7da4def8.chunk.css"
+  },
+  {
+    "revision": "1fcf320b5f87808a14cf",
+    "url": "/static/js/2.ddf4acb6.chunk.js"
+  },
+  {
+    "revision": "670195e9dae107f19334753ff3b65e09",
+    "url": "/static/js/2.ddf4acb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c61b0a9e2f08e67f9a5",
+    "url": "/static/js/main.c14ef20b.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6c47d69a26f95b0e66f9c7010eedac27.js"
+  "/precache-manifest.ce24a0ddc636e9ce884992cc3ae9b8ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
